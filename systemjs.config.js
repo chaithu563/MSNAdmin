e553cb03e17c306d538505b8709c96f8801cbef0
chaitanya edara <chaitanya.edara@aveva.com>
2016-11-23 18:17:46 +0530

    not working jaydata context jaysvcutill.exe added

--- systemjs.config.js
+++ systemjs.config.js
@@ -25,8 +25,8 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      'jaydata/core': 'lib/jaydata/jaydata.min',
-      'jaydata/odata': 'lib/jaydata/jaydataproviders/oDataProvider.min'
+      'jaydata/core': 'lib/jaydata/jaydata.min.js',
+      'jaydata/odata': 'lib/jaydata/jaydataproviders/oDataProvider.min.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
